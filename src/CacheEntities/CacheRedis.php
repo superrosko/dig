bbf95f32a5bdbb45f09f1cb125da1de3305ab0bf
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2019-12-16 16:31:04 +0300

    feat: redis cache add ttl and set data type as hash tables

--- src/CacheEntities/CacheRedis.php
+++ src/CacheEntities/CacheRedis.php
@@ -4,6 +4,7 @@ namespace Superrosko\Dig\CacheEntities;
 
 use \Exception;
 use \Redis;
+use Superrosko\Dig\ResourceRecords\Record;
 
 /**
  * Class CacheRedis
@@ -22,8 +23,15 @@ class CacheRedis extends AbstractCacheEntities
     public function get(string $key)
     {
         if (self::$redis instanceof Redis) {
-            $value = self::$redis->get($key) ?: null;
-            return unserialize($value) ?: null;
+            if ($value = self::$redis->hGetAll($key)) {
+                $records = [];
+                $ttl = self::$redis->ttl($key);
+                foreach ($value as $data => $target_ip) {
+                    $records[] = new Record($key, '', $ttl, 'NS', $data, ['target_ip' => $target_ip]);
+                }
+                return $records;
+            }
+            return null;
         } else {
             throw new Exception('Redis object not found');
         }
@@ -31,11 +39,19 @@ class CacheRedis extends AbstractCacheEntities
 
     /**
      * @inheritDoc
+     * @var Record $record
      */
     public function set(string $key, $value)
     {
         if (self::$redis instanceof Redis) {
-            self::$redis->set($key, serialize($value));
+            $ttl = [];
+            $data = [];
+            foreach ($value as $record) {
+                $ttl[] = $record->ttl;
+                $data[$record->data] = $record->opt['target_ip'];
+            }
+            self::$redis->hMSet($key, $data);
+            self::$redis->expire($key, min($ttl));
         } else {
             throw new Exception('Redis object not found');
         }
