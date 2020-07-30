69687f3efaca64dcef43ef7dc863d86746729019
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-07-31 01:45:18 +0300

    feat(test): update tests config

---
+++ tests/_support/UnitTester.php
@@ -0,0 +1,50 @@
+<?php
+
+use Codeception\Actor;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method void pause()
+ *
+ * @SuppressWarnings(PHPMD)
+ */
+class UnitTester extends Actor
+{
+    use _generated\UnitTesterActions;
+
+    /**
+     * @param string $className
+     * @param string $propertyName
+     * @param object $stub
+     * @return mixed
+     * @throws ReflectionException
+     */
+    public function getPrivatePropertyValue(string $className, string $propertyName, object $stub)
+    {
+        $reflector = new ReflectionClass($className);
+        $property = $reflector->getProperty($propertyName);
+        $property->setAccessible(true);
+        return $property->getValue($stub);
+    }
+
+    /**
+     * @param mixed $expected
+     * @param string $className
+     * @param string $propertyName
+     * @param object $stub
+     * @throws ReflectionException
+     */
+    public function assertPrivatePropertyValue($expected, string $className, string $propertyName, object $stub)
+    {
+        $this->assertEquals($expected, $this->getPrivatePropertyValue($className, $propertyName, $stub));
+    }
+}
