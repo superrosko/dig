a009550de11382c4148d509c62080a12ee68b5cf
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-08-06 02:26:00 +0300

    refactor(test): dig client get executor test

--- tests/unit/DigClientGetExecutorTest.php
+++ tests/unit/DigClientGetExecutorTest.php
@@ -2,77 +2,13 @@
 
 namespace unit;
 
-use ArgumentCountError;
 use Codeception\Test\Unit;
 use ReflectionException;
-use StdClass;
 use Superrosko\Dig\DigClient;
 use Superrosko\Dig\Executor\ExecutorInterface;
-use TypeError;
 
 class DigClientGetExecutorTest extends Unit
 {
-
-    protected function _before()
-    {
-    }
-
-    protected function _after()
-    {
-    }
-
-    /**
-     * Testing ArgumentCountError
-     * @throws ReflectionException
-     */
-    public function testGetExecutorCheckArgumentCount()
-    {
-        $this->expectException(ArgumentCountError::class);
-        DigClient::getExecutor();
-    }
-
-    /**
-     * Testing TypeError
-     * @throws ReflectionException
-     */
-    public function testGetExecutorCheckParameterTypeArray()
-    {
-        $this->expectException(TypeError::class);
-        DigClient::getExecutor([], []);
-    }
-
-    /**
-     * Testing TypeError
-     * @throws ReflectionException
-     */
-    public function testGetExecutorCheckParameterTypeNull()
-    {
-        $this->expectException(TypeError::class);
-        DigClient::getExecutor(null, null);
-    }
-
-    /**
-     * Testing TypeError
-     * @throws ReflectionException
-     */
-    public function testGetExecutorCheckParameterTypeObject()
-    {
-        $this->expectException(TypeError::class);
-        DigClient::getExecutor(new StdClass(), new StdClass());
-    }
-
-    /**
-     * Testing TypeError
-     * @throws ReflectionException
-     */
-    public function testGetExecutorCheckParameterTypeClosure()
-    {
-        $this->expectException(TypeError::class);
-        DigClient::getExecutor(function () {
-        }, function () {
-        });
-    }
-
     /**
      * Testing wrong execution class
      * @throws ReflectionException
