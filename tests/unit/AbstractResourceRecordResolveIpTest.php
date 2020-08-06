5970b207542830d81a41a12378f1d2bb7b8f4292
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-08-07 00:13:07 +0300

    refactor(test): abstract resource record resolve ip test

--- tests/unit/AbstractResourceRecordResolveIpTest.php
+++ tests/unit/AbstractResourceRecordResolveIpTest.php
@@ -2,20 +2,20 @@
 
 namespace unit;
 
-use ArgumentCountError;
 use Codeception\Test\Unit;
-use StdClass;
+use ReflectionException;
 use Superrosko\Dig\ResourceRecords\AbstractResourceRecord;
-use TypeError;
+use UnitTester;
 
 class AbstractResourceRecordResolveIpTest extends Unit
 {
     /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    /**
      * @var $stub AbstractResourceRecord
-     * @method mockGetParamName
-     * @method mockGetParamType
-     * @method mockGetParamServer
-     * @method mockGetParamOpt
      */
     protected $stub;
 
@@ -25,138 +25,17 @@ class AbstractResourceRecordResolveIpTest extends Unit
         $type = DNS_A;
         $server = '127.0.0.1';
         $opt = ['test_data'];
-        $this->stub = $this->getStubClass($name, $type, $server, $opt);
-    }
-
-    protected function _after()
-    {
-    }
-
-    private function getStubClass($name, $type, $server, $opt)
-    {
-        return new class($name, $type, $server, $opt) extends AbstractResourceRecord {
-
-            public function mockGetParamName()
-            {
-                return $this->name;
-            }
-
-            public function mockGetParamType()
-            {
-                return $this->type;
-            }
-
-            public function mockGetParamServer()
-            {
-                return $this->server;
-            }
-
-            public function mockGetParamOpt()
-            {
-                return $this->opt;
-            }
-
-            public function getRequest()
-            {
-            }
-
-            public function parseResponse(array $response, bool $resolve = false)
-            {
-            }
-
-            public function convertType()
-            {
-            }
-
-            public function parseRecord($record)
-            {
-            }
-
-            public function getRecordProps($record)
-            {
-            }
-
-            public function getNS($record, bool $resolve = false)
-            {
-            }
-
-            public function getA($record, bool $resolve = false)
-            {
-            }
-
-            public function getAAAA($record, bool $resolve = false)
-            {
-            }
-
-            public function getTXT($record, bool $resolve = false)
-            {
-            }
-
-            public function getCNAME($record, bool $resolve = false)
-            {
-            }
-
-            public function getPTR($record, bool $resolve = false)
-            {
-            }
-        };
-    }
-
-    /**
-     * Testing ArgumentCountError
-     */
-    public function testResolveIpCheckArgumentCount()
-    {
-
-        $this->expectException(ArgumentCountError::class);
-        $this->stub->resolveIp();
-    }
-
-    /**
-     * Testing TypeError
-     */
-    public function testResolveIpCheckCheckParameterTypeArray()
-    {
-        $this->expectException(TypeError::class);
-        $this->stub->resolveIp([], []);
-    }
-
-    /**
-     * Testing TypeError
-     */
-    public function testResolveIpCheckCheckParameterTypeNull()
-    {
-        $this->expectException(TypeError::class);
-        $this->stub->resolveIp(null, null);
-    }
-
-    /**
-     * Testing TypeError
-     */
-    public function testResolveIpCheckCheckParameterTypeObject()
-    {
-        $this->expectException(TypeError::class);
-        $this->stub->resolveIp(new StdClass(), new StdClass());
-    }
-
-    /**
-     * Testing TypeError
-     */
-    public function testResolveIpCheckCheckParameterTypeClosure()
-    {
-        $this->expectException(TypeError::class);
-        $this->stub->resolveIp(function () {
-        }, function () {
-        });
+        $this->stub = $this->getMockForAbstractClass(AbstractResourceRecord::class, [$name, $type, $server, $opt]);
     }
 
     /**
      * Testing resolve ip with wrong host name
+     * @throws ReflectionException
      */
     public function testResolveIpWithWrongData()
     {
         $this->stub->resolveIp('wrong_data_for_host', true);
-        $targetIp = $this->stub->mockGetParamOpt();
+        $targetIp = $this->tester->getPrivatePropertyValue(AbstractResourceRecord::class, 'opt', $this->stub);
         $this->assertTrue(isset($targetIp['target_ip']));
         $this->assertTrue(filter_var($targetIp['target_ip'], FILTER_VALIDATE_IP) === false);
         $this->assertEquals('wrong_data_for_host', $targetIp['target_ip']);
@@ -164,22 +43,24 @@ class AbstractResourceRecordResolveIpTest extends Unit
 
     /**
      * Testing resolve ip with real host
+     * @throws ReflectionException
      */
     public function testResolveIpWithRealHost()
     {
         $this->stub->resolveIp('example.com', true);
-        $targetIp = $this->stub->mockGetParamOpt();
+        $targetIp = $this->tester->getPrivatePropertyValue(AbstractResourceRecord::class, 'opt', $this->stub);
         $this->assertTrue(isset($targetIp['target_ip']));
         $this->assertTrue(filter_var($targetIp['target_ip'], FILTER_VALIDATE_IP) !== false);
     }
 
     /**
      * Testing resolve ip with real host and not resolve flag
+     * @throws ReflectionException
      */
     public function testResolveIpWithRealHostAndNotResolveFlag()
     {
         $this->stub->resolveIp('example.com', false);
-        $targetIp = $this->stub->mockGetParamOpt();
+        $targetIp = $this->tester->getPrivatePropertyValue(AbstractResourceRecord::class, 'opt', $this->stub);
         $this->assertTrue(!isset($targetIp['target_ip']));
     }
 }
