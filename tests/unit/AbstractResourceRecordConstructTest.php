f0971c79155135e4af3eb486535cb0a19d55bb51
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-08-11 00:15:53 +0300

    refactor(test): abstract resource record construct test

--- tests/unit/AbstractResourceRecordConstructTest.php
+++ tests/unit/AbstractResourceRecordConstructTest.php
@@ -2,147 +2,21 @@
 
 namespace unit;
 
-use ArgumentCountError;
 use Codeception\Test\Unit;
+use ReflectionException;
 use Superrosko\Dig\ResourceRecords\AbstractResourceRecord;
+use UnitTester;
 
 class AbstractResourceRecordConstructTest extends Unit
 {
-
-    protected function _before()
-    {
-    }
-
-    protected function _after()
-    {
-    }
-
-    private function getStubClass($name, $type, $server, $opt)
-    {
-        return new class($name, $type, $server, $opt) extends AbstractResourceRecord {
-
-            public function mockGetParamName()
-            {
-                return $this->name;
-            }
-
-            public function mockGetParamType()
-            {
-                return $this->type;
-            }
-
-            public function mockGetParamServer()
-            {
-                return $this->server;
-            }
-
-            public function mockGetParamOpt()
-            {
-                return $this->opt;
-            }
-
-            public function getRequest()
-            {
-            }
-
-            public function parseResponse(array $response, bool $resolve = false)
-            {
-            }
-
-            public function convertType()
-            {
-            }
-
-            public function parseRecord($record)
-            {
-            }
-
-            public function getRecordProps($record)
-            {
-            }
-
-            public function getNS($record, bool $resolve = false)
-            {
-            }
-
-            public function getA($record, bool $resolve = false)
-            {
-            }
-
-            public function getAAAA($record, bool $resolve = false)
-            {
-            }
-
-            public function getTXT($record, bool $resolve = false)
-            {
-            }
-
-            public function getCNAME($record, bool $resolve = false)
-            {
-            }
-
-            public function getPTR($record, bool $resolve = false)
-            {
-            }
-        };
-    }
-
     /**
-     * Testing ArgumentCountError
+     * @var UnitTester
      */
-    public function testAbstractResourceRecordConstructCheckArgumentCount()
-    {
-        $this->expectException(ArgumentCountError::class);
-        new class() extends AbstractResourceRecord {
-
-            public function getRequest()
-            {
-            }
-
-            public function parseResponse(array $response, bool $resolve = false)
-            {
-            }
-
-            public function convertType()
-            {
-            }
-
-            public function parseRecord($record)
-            {
-            }
-
-            public function getRecordProps($record)
-            {
-            }
-
-            public function getNS($record, bool $resolve = false)
-            {
-            }
-
-            public function getA($record, bool $resolve = false)
-            {
-            }
-
-            public function getAAAA($record, bool $resolve = false)
-            {
-            }
-
-            public function getTXT($record, bool $resolve = false)
-            {
-            }
-
-            public function getCNAME($record, bool $resolve = false)
-            {
-            }
-
-            public function getPTR($record, bool $resolve = false)
-            {
-            }
-        };
-    }
+    protected $tester;
 
     /**
      * Testing construct params
+     * @throws ReflectionException
      */
     public function testAbstractResourceRecordConstructParams()
     {
@@ -150,10 +24,10 @@ class AbstractResourceRecordConstructTest extends Unit
         $type = DNS_A;
         $server = '127.0.0.1';
         $opt = ['test_data'];
-        $stub = $this->getStubClass($name, $type, $server, $opt);
-        $this->assertEquals($name, $stub->mockGetParamName());
-        $this->assertEquals($type, $stub->mockGetParamType());
-        $this->assertEquals($server, $stub->mockGetParamServer());
-        $this->assertEquals($opt, $stub->mockGetParamOpt());
+        $stub = $this->getMockForAbstractClass(AbstractResourceRecord::class, [$name, $type, $server, $opt]);
+        $this->tester->assertPrivatePropertyValue($name, AbstractResourceRecord::class, 'name', $stub);
+        $this->tester->assertPrivatePropertyValue($type, AbstractResourceRecord::class, 'type', $stub);
+        $this->tester->assertPrivatePropertyValue($server, AbstractResourceRecord::class, 'server', $stub);
+        $this->tester->assertPrivatePropertyValue($opt, AbstractResourceRecord::class, 'opt', $stub);
     }
 }
