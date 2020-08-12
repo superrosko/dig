1fccbfe9d2f3d71d87e6b83e468899fca4371029
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-08-13 01:48:05 +0300

    refactor(test): abstract executor set default resolver test

--- tests/unit/AbstractExecutorSetDefaultResolverTest.php
+++ tests/unit/AbstractExecutorSetDefaultResolverTest.php
@@ -2,12 +2,9 @@
 
 namespace unit;
 
-use ArgumentCountError;
 use Codeception\Test\Unit;
-use StdClass;
 use Superrosko\Dig\Exception\DigException;
 use Superrosko\Dig\Executor\AbstractExecutor;
-use TypeError;
 
 class AbstractExecutorSetDefaultResolverTest extends Unit
 {
@@ -25,61 +22,6 @@ class AbstractExecutorSetDefaultResolverTest extends Unit
         $this->stub->expects($this->any())
             ->method('execute')
             ->will($this->returnValue([]));
-    }
-
-    protected function _after()
-    {
-    }
-
-    /**
-     * Testing ArgumentCountError
-     * @throws DigException
-     */
-    public function testSetDefaultResolverCheckArgumentCount()
-    {
-        $this->expectException(ArgumentCountError::class);
-        $this->stub->setDefaultResolver();
-    }
-
-    /**
-     * Testing TypeError
-     * @throws DigException
-     */
-    public function testSetDefaultResolverCheckParameterTypeArray()
-    {
-        $this->expectException(TypeError::class);
-        $this->stub->setDefaultResolver([]);
-    }
-
-    /**
-     * Testing TypeError
-     * @throws DigException
-     */
-    public function testSetDefaultResolverCheckParameterTypeNull()
-    {
-        $this->expectException(TypeError::class);
-        $this->stub->setDefaultResolver(null);
-    }
-
-    /**
-     * Testing TypeError
-     * @throws DigException
-     */
-    public function testSetDefaultResolverCheckParameterTypeObject()
-    {
-        $this->expectException(TypeError::class);
-        $this->stub->setDefaultResolver(new StdClass());
-    }
-
-    /**
-     * Testing TypeError
-     * @throws DigException
-     */
-    public function testSetDefaultResolverCheckParameterTypeClosure()
-    {
-        $this->expectException(TypeError::class);
-        $this->stub->setDefaultResolver(function () {
-        });
     }
 
     /**
